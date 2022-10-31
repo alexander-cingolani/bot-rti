e93027d37a1c95f7ecadb5ed6539f3216460bd91
Alexander Michael Cingolani <alexander.cingolani05@gmail.com>
2022-10-31 10:27:06 +0100

    Refactoring

--- bot/app/components/stats.py
+++ bot/app/components/stats.py
@@ -3,6 +3,11 @@ from app.components.models import Driver
 
 
 def consistency(driver: Driver) -> int:
+    """Returns a number from 0 to 100 calculated on the standard deviation of 
+    the set of finishing positions and taking absences into account. """
+    if not driver.race_results:
+        return 0
+
     race_results = []
     absences = 0
     for race_result in driver.race_results:
@@ -15,6 +20,7 @@ def consistency(driver: Driver) -> int:
 
 
 def speed(driver: Driver) -> int:
+    """Returns an int between 0 and 100 indicating """
     current_category = driver.current_category()
     pole_laptimes = current_category.pole_lap_times()
 
@@ -34,6 +40,10 @@ def speed(driver: Driver) -> int:
 
 
 def experience(driver: Driver, max_races) -> int:
+
+    if not max_races:
+        return 0
+
     disputed_rounds = []
     for race_result in driver.race_results:
         if (
@@ -41,10 +51,16 @@ def experience(driver: Driver, max_races) -> int:
             and race_result.round.round_id not in disputed_rounds
         ):
             disputed_rounds.append(race_result.round.round_id)
+
+    if not len(disputed_rounds):
+        return 0
     return round(100 - (((max_races - len(disputed_rounds)) / max_races * 100) * 0.6))
 
 
 def sportsmanship(driver: Driver) -> int:
+
+    if not driver.race_results:
+        return 0
 
     return round(
         100
@@ -55,6 +71,10 @@ def sportsmanship(driver: Driver) -> int:
 
 
 def race_pace(driver: Driver) -> int:
+
+    if not driver.race_results:
+        return 0
+
     return round(
         100
         - (
@@ -66,13 +86,17 @@ def race_pace(driver: Driver) -> int:
 
 
 def stats(driver: Driver) -> int:
+    """Returns number of wins, podiums and poles and fastest laps achieved by the driver."""
     wins = 0
     podiums = 0
     poles = 0
+
     for rr in driver.race_results:
         if rr.finishing_position == 1:
             wins += 1
         if rr.finishing_position <= 3:
             podiums += 1
+
         poles += rr.fastest_lap_points
+
     return wins, podiums, poles
