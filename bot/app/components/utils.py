dd1cd231989cb73f61580568937e9a6e771e3d35
Alexander <alexander.cingolani05@gmail.com>
2023-05-02 02:40:45 +0200

    Fix typing

--- bot/app/components/utils.py
+++ bot/app/components/utils.py
@@ -1,10 +1,14 @@
 """
 Helper stuff.
 """
-from telegram import Update
+from telegram import Update, InlineKeyboardMarkup
 
 
-async def send_or_edit_message(update: Update, message, reply_markup=None) -> None:
+async def send_or_edit_message(
+    update: Update,
+    message: str,
+    reply_markup: InlineKeyboardMarkup | None = None,
+) -> None:
     if update.callback_query:
         if not reply_markup:
             await update.callback_query.edit_message_text(text=message)
