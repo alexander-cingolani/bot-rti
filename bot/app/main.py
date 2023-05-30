6fdbd5ac94265f81c9b7f4249460299c3579b2c1
Alexander <alexander.cingolani05@gmail.com>
2023-05-30 21:39:38 +0200

    bug fix

--- bot/app/main.py
+++ bot/app/main.py
@@ -386,26 +386,27 @@ async def inline_query(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
             if not unique_teams:
                 unique_teams = "/"
 
-            
             current_category = driver.current_category()
             rnd = current_category.penultimate_completed_round()
             previous_rating = 0
             if rnd:
                 for result in rnd.long_race.race_results:
                     if result.driver_id == driver.driver_id:
-                        previous_rating = result.driver_mu - config.K * result.driver_sigma
+                        if result.driver_mu and result.driver_sigma:
+                            previous_rating = result.driver_mu - config.K * result.driver_sigma
                         break
-            
+
             if previous_rating:
                 diff = round(driver.rating - previous_rating, 2)
                 diff_text = f"↓{abs(diff)}" if diff < 0 else f"↑{abs(diff)}"
                 driver_rating_text = f"<b>Driver Rating</b>: <i>{round(driver.rating, 2)} {diff_text}</i>\n"
             elif driver.rating:
-                driver_rating_text = f"<b>Driver Rating</b>: <i>{round(driver.rating, 2)}</i>\n"
+                driver_rating_text = (
+                    f"<b>Driver Rating</b>: <i>{round(driver.rating, 2)}</i>\n"
+                )
             else:
                 driver_rating_text = f"<b>Driver Rating</b>: <i>N.D.</i>\n"
-            
-            
+
             consistency = driver.consistency()
             speed = driver.speed()
             sportsmanship = driver.sportsmanship()
@@ -417,8 +418,8 @@ async def inline_query(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
                 input_message_content=InputTextMessageContent(
                     (
                         f"<i><b>PROFILO PILOTA: {driver.psn_id.upper()}</b></i>\n\n"
-                        + driver_rating_text +
-                        f"<b>Affidabilità</b>: <i>{consistency if consistency else 'Dati insufficienti'}</i>\n"
+                        + driver_rating_text
+                        + f"<b>Affidabilità</b>: <i>{consistency if consistency else 'Dati insufficienti'}</i>\n"
                         f"<b>Sportività</b>: <i>{sportsmanship if sportsmanship else 'Dati insufficienti'}</i>\n"
                         f"<b>Qualifica</b>: <i>{speed if speed else 'Dati insufficienti'}</i>\n"
                         f"<b>Passo gara</b>: <i>{race_pace if race_pace else 'Dati insufficienti.'}</i>\n\n"
@@ -1023,22 +1024,25 @@ async def user_stats(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     if rnd:
         for result in rnd.long_race.race_results:
             if result.driver_id == driver.driver_id:
-                previous_rating = result.driver_mu - config.K * result.driver_sigma
+                if result.driver_mu and result.driver_sigma:
+                    previous_rating = result.driver_mu - config.K * result.driver_sigma
                 break
-    
     if previous_rating:
         diff = round(driver.rating - previous_rating, 2)
         diff_text = f"↓{abs(diff)}" if diff < 0 else f"↑{abs(diff)}"
         driver_rating_text = f"<b>Driver Rating</b>: <i>{round(driver.rating, 2)} {diff_text}</i>\n"
     elif driver.rating:
-        driver_rating_text = f"<b>Driver Rating</b>: <i>{round(driver.rating, 2)}</i>\n"
+        driver_rating_text = (
+            f"<b>Driver Rating</b>: <i>{round(driver.rating, 2)}</i>\n"
+        )
     else:
         driver_rating_text = f"<b>Driver Rating</b>: <i>N.D.</i>\n"
 
+
     await update.message.reply_text(
         f"<i><b>PROFILO PILOTA: {driver.psn_id.upper()}</b></i>\n\n"
-        + driver_rating_text +
-        f"<b>Affidabilità</b>: <i>{consistency if consistency else 'Dati insufficienti'}</i>\n"
+        + driver_rating_text
+        + f"<b>Affidabilità</b>: <i>{consistency if consistency else 'Dati insufficienti'}</i>\n"
         f"<b>Sportività</b>: <i>{sportsmanship if sportsmanship else 'Dati insufficienti'}</i>\n"
         f"<b>Qualifica</b>: <i>{speed if speed else 'Dati insufficienti'}</i>\n"
         f"<b>Passo gara</b>: <i>{race_pace if race_pace else 'Dati insufficienti.'}</i>\n\n"
