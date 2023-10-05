0f60b5636e981be958bc099f521ecb1cac90c08c
Alexander <alexander.cingolani05@gmail.com>
2023-10-05 17:01:37 +0100

    Add JWT auth for /rre-results-upload

---
+++ api/app/components/auth.py
@@ -0,0 +1,99 @@
+from datetime import datetime, timedelta
+import logging
+import os
+from typing import Annotated, Any
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+SECRET_KEY = os.environ.get("SECRET_API_KEY")
+HASHED_USER_PASSWORD = os.environ.get("HASHED_USER_PASSWORD")
+USERNAME = os.environ.get("RRE_SERVER_USERNAME")
+ALGORITHM = "HS256"
+
+if not HASHED_USER_PASSWORD or not SECRET_KEY or not USERNAME:
+    raise RuntimeError("Environment variables not set correctly")
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def get_user(username: str):
+    return UserInDB(
+        username=username,
+        hashed_password=HASHED_USER_PASSWORD,
+    )
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    logging.info(user.username)
+    logging.info(user.hashed_password)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict[str, Any], expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    logging.info(to_encode)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")  # type: ignore
+        if not username:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)  # type: ignore
+    if not user:
+        raise credentials_exception
+    return user
