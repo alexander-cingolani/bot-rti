1b508228924b38c02c01b552e5976f431a905dc7
Alexander <alexander.cingolani05@gmail.com>
2023-10-30 15:59:16 +0000

    Change logging level

--- bot/app/main.py
+++ bot/app/main.py
@@ -62,7 +62,7 @@ from queries import (
 )
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.ERROR
 )
 logger = logging.getLogger(__name__)
 
