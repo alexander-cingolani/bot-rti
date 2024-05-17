8ebfe9b5923abe371709f13b8e78830fb5518849
alexander-cingolani <alexander-cingolani@users.noreply.github.com>
2024-05-17 21:10:56 +0000

    Format code with black

--- bot/app/config.py
+++ bot/app/config.py
@@ -58,7 +58,7 @@ ADMIN_ONLY_COMMANDS = (
     ("salva_risultati", "Salva i risultati delle ultime gare."),
     ("penalizza", "Applica una penalità per un'infrazione commessa da un pilota."),
     ("aggiungi_watermark", "Aggiungi il logo RTI alle foto."),
-    ("annulla_penalita", "Annulla una penalità.")
+    ("annulla_penalita", "Annulla una penalità."),
 )
 ADMIN_COMMANDS = LEADER_COMMANDS + ADMIN_ONLY_COMMANDS
 
