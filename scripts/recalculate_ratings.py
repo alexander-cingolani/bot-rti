0c8bc39ccfd02867cca8e1a6c6724ce1ff27dd66
Alexander <alexander.cingolani05@gmail.com>
2024-06-17 21:56:11 +0200

    Add recalculate_all_ratings script

--- scripts/recalculate_ratings.py
+++ scripts/recalculate_ratings.py
@@ -3,11 +3,15 @@ This module contains the driver ranking function.
 """
 
 from decimal import Decimal
+import os
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import trueskill as ts  # type: ignore
 
 from models import Driver, RaceResult
+from queries import get_championship, get_drivers
 
 TrueSkillEnv = ts.TrueSkill(
     draw_probability=0,
@@ -101,7 +105,70 @@ def recalculate_ratings():
 
     for driver in drivers:
         print(f"{driver.psn_id}: {driver.mu} - {driver.sigma}")
+        
+        
+def recalculate_all_ratings():
+    """Only used to recalculate all the ratings in the last championship."""
+    sqla_session = DBSession()
+    
+    drivers = get_drivers(sqla_session)
+    for driver in drivers:
+        driver.mu = 25
+        driver.sigma = 25/3
+    sqla_session.commit()
+
+    for i in range(1, 6):
+        championship = get_championship(sqla_session, championship_id=i)
+        for category in championship.categories:
+            for round in category.rounds:
+                for session in round.sessions:
+                    if session.is_quali:
+                        continue
+
+                    initial_ratings: list[tuple[ts.Rating]] = []
+                    finishing_positions: list[int] = []
+                    race_results: list[RaceResult] = []
+                    for result in session.race_results:
+                        if result.participated:
+                            rtg = (
+                                ts.Rating(
+                                    mu=float(result.driver.mu),
+                                    sigma=float(result.driver.sigma),
+                                ),
+                            )
+                            initial_ratings.append(rtg)
+                            finishing_positions.append(result.position)
+                            race_results.append(result)
+                            result.mu = rtg[0].mu
+                            result.sigma = rtg[0].sigma
+                    print(initial_ratings)
+                    print(finishing_positions)
+                    if initial_ratings:
+                        new_ratings = ts.rate(initial_ratings, finishing_positions)
+                        for i, result in enumerate(race_results):
+                            result.driver.mu = Decimal.from_float(
+                                new_ratings[i][0].mu
+                            ).quantize(Decimal("1.000000"))
+                            result.driver.sigma = Decimal.from_float(
+                                new_ratings[i][0].sigma
+                            ).quantize(Decimal("1.000000"))
+
+        sqla_session.commit()
+        sqla_session.expire_all()
+
+        championship = get_championship(sqla_session)
+
+        if not championship:
+            return
+
+        drivers = championship.driver_list
+
+        drivers.sort(key=lambda x: x.rating if x.rating else 0, reverse=True)
+
+        for driver in drivers:
+            print(f"{driver.psn_id}: {driver.mu} - {driver.sigma}")
+        
 
 
 if __name__ == "__main__":
-    recalculate_ratings()
+    recalculate_all_ratings()
