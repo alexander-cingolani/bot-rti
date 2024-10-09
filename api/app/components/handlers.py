7e3ee105a6ad126d569fe861f14a80bdc00f225b
alexander-cingolani <alexander-cingolani@users.noreply.github.com>
2024-10-09 04:40:50 +0000

    Format code with black

--- api/app/components/handlers.py
+++ api/app/components/handlers.py
@@ -176,7 +176,7 @@ def get_standings_with_results(
 
     results = category.standings_with_results()
 
-    standings:list[DriverSummary] = []
+    standings: list[DriverSummary] = []
     if not results:
         for driver in category.active_drivers():
             team = driver.driver.current_team()
